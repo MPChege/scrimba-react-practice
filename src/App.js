cc84114099b759ab5df9a8c19e41793f8f4bb399
MPChege <mark.wanyoike@strathmore.edu>
2023-04-25 21:24:33 +0300

    PASSED FIRST EXERCISE

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@
 import './App.css';
 
 function App() {
-  // const name= <h1>MARK</h1>
-  // const age= <h1>21</h1>
-  // const email=<h1>Markpaulcheg77@gmail.com</h1>
-  // const phone=<h1>+1-908-908-908</h1>
-  // const user =(
-  // <div>
-  // {name}
-  //  {age}
-  //  {email}
-  //  {phone}
-  //  </div>
-  //  );
-
-  return (
-    <div className="App">
-     
-    <User name ="Mark Paul" age ={21} email ="markpaulchege77@gmai.com" />
-    {/* <User />
-    <User /> */}
-
-    </div>
-  );
+return(
+  <div className='App'>
+<Job salary= {90000} position = "Senior SDE" company = "Amazon"/>
+<Job salary= {120000} position = "Junior SDE" company = "Google"/>
+<Job salary= {10000} position = "Project Manager" company = "Netflix"/>
+  </div>
+);
 }
 
-const User = (props) =>{
-  return  (
-  <div>
-  <h1>{props.name}</h1>
-  <h1>{props.age}</h1>
-  <h1>{props.email}</h1>
-   </div>
-   );
-
+const Job = (props) => {
+  return(
+    <div>
+      <h1> {props.salary}</h1>
+      <h1> {props.position}</h1>
+      <h1> {props.company}</h1>
+       </div>
+  );
 };
 export default App;
