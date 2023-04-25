5cdbeba4bbaa585e98a3897feb73c8e77e5782f3
MPChege <mark.wanyoike@strathmore.edu>
2023-04-25 20:54:20 +0300

    calling components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@
 import './App.css';
 
 function App() {
-  const name= <h1>MARK</h1>
+  // const name= <h1>MARK</h1>
+  // const age= <h1>21</h1>
+  // const email=<h1>Markpaulcheg77@gmail.com</h1>
+  // const phone=<h1>+1-908-908-908</h1>
+  // const user =(
+  // <div>
+  // {name}
+  //  {age}
+  //  {email}
+  //  {phone}
+  //  </div>
+  //  );
+
   return (
     <div className="App">
-     {name}
-     {name}
      
+    <User />
+    <User />
+    <User />
+
     </div>
   );
 }
 
+const User = () =>{
+  return  (
+  <div>
+  <h1>MARK</h1>
+  <h1>21</h1>
+  <h1>Markpaulcheg77@gmail.com</h1>
+   </div>
+   );
+
+};
 export default App;
